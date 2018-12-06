b6bd49b550f726f7df2c6a29585dee2c8b5fc6d4
micascheid <mica.linscheid@western.edu>
2018-12-06 13:20:23 -0700

    Basic setup lets go

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
+# Imports Obviously
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, \
     login_user, logout_user, current_user, login_required
 
 
+
+# Basic Setup shiz
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['SECRET_KEY']='MyKeyIsTheDankest'
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 
+
+
+# Routes
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+
+# Table/Models
 class HealthyRecipe(db.Model):
     id =            db.Column(db.Integer, primary_key=True)
     ingredients =   db.Column(db.String(1000))
@@ -33,7 +44,11 @@ class User(UserMixin, db.Model):
     name     = db.Column(db.String(40), nullable=False)
 
 
+@login_manager.user_loader
+def load_user(uid):
+    user = User.query.get(uid)
+    return user
+
+
 if __name__=="__main__":
-    login_manager = LoginManager(app)
-    login_manager.init_app(app)
     app.run()
